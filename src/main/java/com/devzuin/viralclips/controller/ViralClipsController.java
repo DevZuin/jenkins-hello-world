f28ef95f2edee3f94f44e4be642dff3f0de01040
Enay Zuin <enayzuin@gmail.com>
2024-12-10 20:34:55 -0300

    first commit

---
+++ src/main/java/com/devzuin/viralclips/controller/ViralClipsController.java
@@ -0,0 +1,18 @@
+package com.devzuin.viralclips.controller;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ViralClipsController {
+
+    @GetMapping("/response")
+    public Map<String, String> getResponse() {
+        return Collections.singletonMap("response", "success");
+    }
+}
